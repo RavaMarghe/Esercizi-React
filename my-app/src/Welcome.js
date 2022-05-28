744163e97e7455459b858074ef44b9ea386f13b0
RavaMarghe <margherita.ravanello@gmail.com>
2022-05-28 16:25:43 +0200

    modified condition of the name prop

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
         return(
             <div>
                 <p>Welcome, <strong>{this.props.name}</strong>!</p>
-                 {this.props.age > 18 && this.props.age < 65 && <Age age={this.props.age}/>}
+                 {this.props.name === 'Jhon' && this.props.age > 18 && this.props.age < 65 && <Age age={this.props.age}/>}
             </div>
             // perchè questo no?
             //{(65 > this.props.age > 18) && <Age age={this.props.age}/>}
