bd44946503face23370e9f82b500dee73fc185a9
RavaMarghe <margherita.ravanello@gmail.com>
2022-05-28 16:10:14 +0200

    modified condition of age prop

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
         return(
             <div>
                 <p>Welcome, <strong>{this.props.name}</strong>!</p>
-                {this.props.age > 18 && <Age age={this.props.age}/>} 
+                {!!this.props.age && <Age age={this.props.age}/>} 
             </div>
         )
     }
