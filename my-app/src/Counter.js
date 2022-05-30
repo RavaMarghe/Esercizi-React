00ca4da5edf0345bccca4dfb9a2d134a20a41814
RavaMarghe <margherita.ravanello@gmail.com>
2022-05-30 14:50:02 +0200

    feat: add CounterDisplay.js

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,33 +1,30 @@
 import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
-    state = {
-        count: this.props.initialValue,
-    }
+  state = {
+    count: this.props.initialValue,
+  };
 
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        setInterval(() => {
-            this.setState((state) => {
-                return {
-                    count: state.count + this.props.incrementBy
-                }
-            })
-        }, this.props.timeout)
-    }
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + this.props.incrementBy,
+        };
+      });
+    }, this.props.timeout);
+  }
 
-    render() {
-        return (
-            <>
-            <h1>Count: {this.state.count}</h1>
-            </>
-        )
-    }
+  render() {
+    return <><CounterDisplay state={this.state.count}/></>;
+  }
 }
 
 Counter.defaultProps = {
-    initialValue: 0,
-    incrementBy: 1,
-    timeout: 1000,
-}
+  initialValue: 0,
+  incrementBy: 1,
+  timeout: 1000,
+};
