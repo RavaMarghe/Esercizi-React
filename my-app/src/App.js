e8ad96a926b37d868192f1124c64c21b413aaf77
Margehrita Ravanello <margherita.ravanello@gmail.com>
2022-06-16 17:34:00 +0200

    feat: add ClickTracker.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
 import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
@@ -12,6 +13,7 @@ export class App extends React.Component {
                 <Welcome />
                 <Counter />
                 <ClickCounter />
+                <ClickTracker />
             </div>
         )
     }
