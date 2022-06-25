fc50da991db4f96864846f852627d446ae648b56
Margehrita Ravanello <margherita.ravanello@gmail.com>
2022-06-25 15:33:44 +0200

    Welcome.js  rewritten

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,12 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
 import { Container } from "./Container";
-import { Counter } from "./Counter";
-import { Hello } from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { ToDoList } from "./ToDoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
   render() {
     return (
       <Container title="Esercizi react">
-        <InteractiveWelcome />
-        <Hello />
-        <hr />
-        <Counter />
-        <hr />
-        <ClickCounter />
-        <hr />
-        <ClickTracker />
-        <hr />
-        <Login />
-        <hr />
-        <UncontrolledLogin />
-        <hr />
-        <ToDoList
-          render={(items, handleRemoveButton) => {
-            return (
-              <div>
-                <ul>
-                  {items.map((item, index) => (
-                    <div>
-                      <li key={item + index}>{item}</li>
-                      <button onClick={handleRemoveButton}>Remove item</button>
-                    </div>
-                  ))}
-                </ul>
-              </div>
-            );
-          }}
-        ></ToDoList>
+        <Welcome name="Marghe" age={23} />
       </Container>
     );
   }
