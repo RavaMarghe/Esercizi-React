a6be43b874319c551ba4fa9d7d1697dcb1d02de8
Margehrita Ravanello <margherita.ravanello@gmail.com>
2022-06-29 15:21:25 +0200

    feat: add useForm.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { Container } from "./Container";
-import { useCounter } from "./useCounter";
+import { useForm } from "./useForm";
 
 /*function onCounterChange(count){
   console.log(`The Counter is now: ${count}`)
 }*/
 
+export function App() {
 
-export function App () {
-  const {counter, onIncrement} = useCounter()
-
-    return (
-      <Container title="Esercizi react">
-        <button onClick={onIncrement}>{counter}</button>
-      </Container>
-    );
-  }
-
+  return (
+    <Container title="Esercizi react">
+    </Container>
+  );
+}
