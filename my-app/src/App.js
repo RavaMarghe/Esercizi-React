b5b1b7941ef1ff5b136da88ecec19cb8ae55e681
Margehrita Ravanello <margherita.ravanello@gmail.com>
2022-06-29 13:22:17 +0200

    feat: add GithubUser.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ClickCounter } from "./ClickCounter";
 import { Container } from "./Container";
+import { GithubUser } from "./GithubUser";
 
 function onCounterChange(count){
   console.log(`The Counter is now: ${count}`)
@@ -10,7 +11,7 @@ export class App extends React.Component {
   render() {
     return (
       <Container title="Esercizi react">
-        <ClickCounter initialValue={0} onCounterChange={onCounterChange}/>
+        <GithubUser username={"RavaMarghe"} />
       </Container>
     );
   }
