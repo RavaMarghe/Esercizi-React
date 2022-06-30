a2196a9bd6a3f5f6aed0da3bcb1a3407bbda18ff
Margehrita Ravanello <margherita.ravanello@gmail.com>
2022-06-30 19:58:06 +0200

    feat: add page not found

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,6 +15,14 @@ export function App() {
         <Route path="/" element={<Welcome name={"Marghe"} />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/users/:username" element={<ShowGithubUser />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>Ppage not found</h1>
+            </div>
+          }
+        />
       </Routes>
       <Link to="/">Welcome</Link> | <Link to="/counter">Counter</Link> |
       <Link to="/users/:username">Github user</Link>
