1b9b3d35e0aac1eaea8bdb780b14c8df4b7f52c9
Margehrita Ravanello <margherita.ravanello@gmail.com>
2022-06-30 17:32:40 +0200

    add path to Counter.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 
 /*function onCounterChange(count){
   console.log(`The Counter is now: ${count}`)
@@ -11,6 +12,7 @@ export function App() {
     <div>
       <Routes>
         <Route path="/" element={<Welcome name={"Marghe"} />} />
+        <Route path="/counter" element={<Counter  />} />
       </Routes>
     </div>
   );
